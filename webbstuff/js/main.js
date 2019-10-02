d189b6886bbacb4fa49a2ec8154f12b00cfe54a5
Oliver <oliver.jam@elev.ga.ntig.se>
2019-10-02 13:37:53 +0200

    update

--- webbstuff/js/main.js
+++ webbstuff/js/main.js
@@ -7,15 +7,20 @@ imgs[0].style.opacity = opacity;
 imgs.forEach(img => img.addEventListener('click', imgClick));
 
 function imgClick(e) {
-    imgs.forEach(img => (img.style.opacity = 1));
+    let el = document.querySelector(e.target);
+    if(el.nodeName == "IMG"){
+        imgs.forEach(img => (img.style.opacity = 1));
 
-    //Btyder bilden till bilden man har klickat på
-    current.src = e.target.src;
+        //Btyder bilden till bilden man har klickat på
+        current.src = e.target.src;
+    
+        current.classList.add('fade-in');
+    
+        setTimeout(() => current.classList.remove('fade-in'),500);
+    
+        //btyder opacitin
+        e.target.style.opacity = opacity;
 
-    current.classList.add('fade-in');
-
-    setTimeout(() => current.classList.remove('fade-in'),500);
-
-    //btyder opacitin
-    e.target.style.opacity = opacity;
+        console.log(e.target);
+    }
 }
